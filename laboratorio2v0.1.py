9dc7dc969a400da08040c2624847cc6e08d62476
herbertdubon <hdubon@tecnasa.com>
2020-10-25 21:16:27 -0600

    Se crearon funcionas para hacer el código modular

--- laboratorio2v0.1.py
+++ laboratorio2v0.1.py
@@ -1,58 +1,88 @@
 
-import errno
-import os
+import sys
 from tqdm import tqdm
 import time as time
 
-while 1:
-    print('Seleccione una opcion para continuar: ')
-    print('1. Especifique el nombre de archivo para procesar su contenido: ')
-    print('2. Salir del programa')
+def salir():
+    sys.exit()
+
+#Se creo la función de menu con las opciones que el usuario puede elegir. Al final se le envia la opcion elegida 
+# a la función lectura. 
+def menu():
+    print("""
+    Seleccione una opcion para continuar:
+    1. Especifique el nombre de archivo para procesar su contenido incluyendo su extensión (Ej: archivo.txt) 
+    2. Salir del programa
+        """)
     opcion = int(input('\nCuál opcion desea realizar?\n'))
+    lectura(opcion)
     
-    if opcion == 1:
-        archivo = input('\nIngrese el nombre del archivo: ')
-        try:
-            with open(archivo) as archivo:  
-                inicio = time.time()
-                print('El archivo ingresado se ha podido leer satisfactoriamente.\n')
-                contenido = archivo.read()
-                
-                #Numero de caracteres que hay en el archivo ingresado
-                numero_caracteres = len(contenido)
-                print('Hay {} caracteres en el archivo de texto.'.format(numero_caracteres))
-                
-                #Numero de palabras que hay en el archivo ingresado
-                numero_palabras = contenido.split()
-                print('Hay {} palabras en el archivo de texto.'.format(len(numero_palabras)))
-                
-                #Numero de lineas que hay en el archivo
-                numero_lineas = len(contenido.splitlines())
-                print('Hay {} lineas en el archivo de texto.'.format(numero_lineas)) 
-                
-                #Numero de palabras unicas que hay en el archivo
-                lineas = contenido.split()
-                nombres = []
-                nombres_unicos = []
-                for i in (lineas):
-                    nombres.append(i)
-                for j in tqdm(nombres):
-                    if j not in nombres_unicos:
-                        nombres_unicos.append(j)
-                #count = 0
-                #for k in tqdm(nombres_unicos):
-                #    count += 1
-                print('Hay {} nombres unicos en el archivo de texto.'.format(len(nombres_unicos)))
-                #print('Hay {} nombres unicos en el archivo de texto.'.format(count))
-                
-                #Tiempo de ejecución programa
-                print('Duracion: {} segundos'.format(time.time() - inicio))                       
-                
-                menu = input('\nDesea analizar otro archivo? (s/n)')
-                if menu== "n":
-                    break                       
-        except FileNotFoundError:
-            print('No existe el archivo {} en la carpeta.\n'.format(archivo))
-            menu = input('\nDesea analizar otro archivo? (s/n)')
-            if menu== "n":
-                break
+#Se creo la función lectura a la cual se le pasa el parametro de la opcion elegida. Si es 1 procede a la lectura del
+#archivo y si es 2 se sale del programa. A la función resultados se le pasa el parametro del nombre_archivo digitado por el usuario. 
+def lectura(opcion):
+    try:
+        while True:
+            if opcion == 1:      
+                nombre_archivo = input('\nIngrese el nombre del archivo: ')
+                resultados(nombre_archivo)
+                print('El nombre_archivo ingresado se ha podido leer satisfactoriamente.\n')
+            else:
+                salir()
+        
+    except FileNotFoundError:
+        print('El archivo no existe en la carpeta')
+        opcion = input('\nDesea analizar otro archivo? (s/n)')
+        if opcion == "s":
+            menu()                               
+        elif (opcion == "n"):
+            salir()
+            
+    except ValueError:
+        print('Opción digitada no válida')
+        opcion = input('\nDesea analizar otro archivo? (s/n)')
+        if opcion == "s":
+            menu()                               
+        elif (opcion == "n"):
+            salir()
+
+#Se crea la fución resultados a la cual se le pasa el parametro del nombre del archivo y se procesan los 4 resultados solicitados.
+def resultados(nombre_archivo):
+    while True:                   
+        with open(nombre_archivo) as nombre_archivo:  
+            inicio = time.time()            
+            #La variable contenido hace referencia a la información dentro del archivo de texto.
+            contenido = nombre_archivo.read()
+            print('El nombre_archivo ingresado se ha podido leer satisfactoriamente.\n')
+            
+        #Numero de caracteres que hay en el archivo ingresado
+        numero_caracteres = len(contenido)
+        print('Hay {} caracteres en el archivo de texto.'.format(numero_caracteres))
+        
+        #Numero de palabras que hay en el archivo ingresado
+        numero_palabras = contenido.split()
+        print('Hay {} palabras en el archivo de texto.'.format(len(numero_palabras)))
+        
+        #Numero de lineas que hay en el archivo
+        numero_lineas = len(contenido.splitlines())
+        print('Hay {} lineas en el archivo de texto.'.format(numero_lineas)) 
+        
+        #Numero de palabras unicas que hay en el archivo
+        lineas = contenido.split()
+        palabras = []
+        palabras_unicas = []
+        for i in (lineas):
+            palabras.append(i)
+        for j in tqdm(palabras):
+            if j not in palabras_unicas:
+                palabras_unicas.append(j)
+        print('Hay {} nombres unicos en el archivo de texto.'.format(len(palabras_unicas)))
+        
+        #Tiempo de ejecución programa
+        print('Duracion: {} segundos'.format(time.time() - inicio))                       
+        
+        opcion = input('\nDesea analizar otro archivo? (s/n)')
+        if opcion == "s":
+            menu()                               
+        elif (opcion == "n"):
+            salir()            
+menu()  
