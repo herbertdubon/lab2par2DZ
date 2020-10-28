662b23f4d481c134b9ea2e38682182fd08b75704
herbertdubon <hdubon@tecnasa.com>
2020-10-28 08:09:43 -0600

    Se agregan comentarios al codigo para mejor entendimiento y documentacion.

--- menu.py
+++ menu.py
@@ -2,9 +2,11 @@ import sys
 import numpy as np
 import time
 
+#Se creo función salir para cerrar el programa
 def salir():
     sys.exit()
     
+#Se creo la funcion inputNumero para el ingreso por teclado de la opcion    
 def inputNumero(opcion):
     while True: 
         try:
@@ -15,6 +17,8 @@ def inputNumero(opcion):
             pass        
     return numero
 
+#Se creo la función de menu con las opciones que el usuario puede elegir. 
+#Al final se le envia la opcion elegida a la función lectura. 
 def menu(opciones):
     for i in range(len(opciones)):
         print("{:d}. {:s}".format(i+1, opciones[i]))
@@ -24,6 +28,10 @@ def menu(opciones):
         eleccion = inputNumero("Favor elija una opcion: ")    
         lectura(eleccion)          
 
+#Se creo la función lectura a la cual se le pasa el parametro de la 
+#opcion elegida. Si es 1 procede a la lectura del archivo y si es 2 se
+#sale del programa. A la función resultados se le pasa el parametro del
+#nombre_archivo digitado por el usuario. 
 def lectura(eleccion):
     try:
         while True:
@@ -32,18 +40,25 @@ def lectura(eleccion):
                 resultados(nombre_archivo)
             elif eleccion == 2:
                 salir()
-            
+    #Si no encuentra el nombre del archivo devuele un mensaje de error.         
     except FileNotFoundError:
         print('El archivo no existe en la carpeta')
         lectura(1)                               
-            
+    
+    #Si el usuario escribe una opcion no valida devuelve mensaje error.        
     except ValueError:
         print('Opción digitada no válida')
         lectura(1)
 
+#Se crea la fución resultados a la cual se le pasa el parametro del 
+#nombre del archivo y se procesan los 4 resultados solicitados.
 def resultados(nombre_archivo):
     while True:
-        inicio = time.time()        
+        #Se inicia el tiempo de ejecucion del programa
+        inicio = time.time()
+        
+        #El programa soporta la lectura de archivos json, txt, sin 
+        #nombre de extensión y otros.         
         with open(nombre_archivo) as nombre_archivo: 
             contenido = nombre_archivo.read() 
             
@@ -69,11 +84,10 @@ def resultados(nombre_archivo):
             print('Hay {} nombres unicos en el archivo de texto.'
             .format(np.asarray(len(palabras_unicas))))
 
-            #Tiempo de ejecución programa
+            #Tiempo de ejecución programa finaliza
             print('Duracion: {} segundos'.format(time.time() - inicio))  
 
             #Consulta por si el usuario desea analizar otro archivo. 
-            #Esta opción lo regresa al menu principal de
             otroArchivo = input('\nDesea analizar otro archivo? (s/n)')
             if otroArchivo == "s":
                 lectura(1)                              
